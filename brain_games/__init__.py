a9a20a24ae866e5c1aa79efe9ed9b6329c60b6fc
Kroch4ka <nekitgo80@gmail.com>
2020-09-13 13:58:03 +0300

    testing_lint

---
+++ brain_games/__init__.py
@@ -0,0 +1,3 @@
+# creating package for import module.
+
+"""Create a package in the given directory to import the cli.py module."""
--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,13 @@
+# Module with input function
 
+"""The welcome user function asks for a name and displays a greeting."""
+
+import prompt
+
+# The function asks for your name and greets
+
+"""The function takes as input a name of type string and outputs greeting"""
 
 def welcome_user():
-    name = input('May I have your name? ')
-    print("Hello, {}!".format(name))
-
-
+    name = prompt.string('May I have your name? ')
+    print(f"Hello, {name} !")
